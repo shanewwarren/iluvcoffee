7b09fdd331989c5df90fabcb840d54d5bd06ddd1
Shane Warren <shanewwarren@gmail.com>
2021-02-03 04:52:27 -0700

    Add coffee entity

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -6,7 +6,7 @@ import {
   Param,
   Patch,
   Post,
-  Query,
+  // Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 
@@ -15,28 +15,28 @@ export class CoffeesController {
   constructor(private readonly coffeeService: CoffeesService) {}
 
   @Get()
-  findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `This action returns all coffees. Limit ${limit}, offset: ${offset}`;
+  findAll(/*@Query() paginationQuery*/) {
+    // const { limit, offset } = paginationQuery;
+    return this.coffeeService.findAll();
   }
 
   @Get(':id')
   findOne(@Param('id') id: string) {
-    return `This action returns #${id} coffee`;
+    return this.coffeeService.findOne(id);
   }
 
   @Post()
   create(@Body() body) {
-    return body;
+    return this.coffeeService.create(body);
   }
 
   @Patch(':id')
   update(@Param('id') id: string, @Body() body) {
-    return `This action updates #${id} coffee`;
+    return this.coffeeService.update(id, body);
   }
 
   @Delete(':id')
   remove(@Param('id') id: string) {
-    return `This action removes #${id} coffee`;
+    return this.coffeeService.remove(id);
   }
 }
